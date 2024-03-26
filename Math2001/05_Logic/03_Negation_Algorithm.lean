e4515ce2c2852b95f7b4717b8fe5e220383a211e
Yupo "Luke" Chen <799708+lukechen526@users.noreply.github.com>
2024-03-26 22:39:17 +0000

    completed 5.3

--- Math2001/05_Logic/03_Negation_Algorithm.lean
+++ Math2001/05_Logic/03_Negation_Algorithm.lean
@@ -20,7 +20,13 @@ example (P Q : Prop) : ¬ (P ∧ Q) ↔ (¬ P ∨ ¬ Q) := by
       contradiction
     · left
       apply hP
-  · sorry
+  · intro h
+    intro hPQ
+    obtain hnP | hnQ := h
+    · obtain ⟨hP, hQ⟩ := hPQ
+      contradiction
+    · obtain ⟨hP, hQ⟩ := hPQ
+      contradiction
 
 example :
     ¬(∀ m : ℤ, m ≠ 2 → ∃ n : ℤ, n ^ 2 = m) ↔ ∃ m : ℤ, m ≠ 2 ∧ ∀ n : ℤ, n ^ 2 ≠ m :=
@@ -32,7 +38,11 @@ example :
 
 example : ¬(∀ n : ℤ, ∃ m : ℤ, n ^ 2 < m ∧ m < (n + 1) ^ 2)
     ↔ ∃ n : ℤ, ∀ m : ℤ, n ^ 2 ≥ m ∨ m ≥ (n + 1) ^ 2 :=
-  sorry
+  calc ¬(∀ n : ℤ, ∃ m : ℤ, n ^ 2 < m ∧ m < (n + 1) ^ 2)
+    ↔ ∃ n : ℤ, ¬(∃ m : ℤ, n ^ 2 < m ∧ m < (n + 1) ^ 2) := by rel [not_forall]
+    _ ↔ ∃ n : ℤ, ∀ m : ℤ, ¬ (n ^ 2 < m ∧ m < (n + 1) ^ 2) := by rel [not_exists]
+    _ ↔ ∃ n : ℤ, ∀ m : ℤ, ¬ (n ^ 2 < m) ∨ ¬ (m < (n + 1) ^ 2) := by rel [not_and_or]
+    _ ↔ ∃ n : ℤ, ∀ m : ℤ, n ^ 2 ≥ m ∨ m ≥ (n + 1) ^ 2 := by rel [not_lt]
 
 #push_neg ¬(∀ m : ℤ, m ≠ 2 → ∃ n : ℤ, n ^ 2 = m)
   -- ∃ m : ℤ, m ≠ 2 ∧ ∀ (n : ℤ), n ^ 2 ≠ m
@@ -55,23 +65,80 @@ example : ¬ (∃ n : ℕ, n ^ 2 = 2) := by
     calc
       n ^ 2 ≤ 1 ^ 2 := by rel [hn]
       _ < 2 := by numbers
-  · sorry
+  · apply ne_of_gt
+    calc
+      n ^ 2 ≥ 2 ^ 2 := by rel [hn]
+      _ = 4 := by numbers
+      _ > 2 := by numbers
+
+example (P : α → Prop) : ¬ (∃ x, P x) ↔ ∀ x, ¬ P x := by
+  constructor
+  · intro h a ha
+    have : ∃ x, P x
+    · use a
+      apply ha
+    contradiction
+  · intro h h'
+    obtain ⟨x, hx⟩ := h'
+    have : ¬ P x := h x
+    contradiction
 
 /-! # Exercises -/
 
 
 example (P : Prop) : ¬ (¬ P) ↔ P := by
-  sorry
+  constructor
+  · intro hnP
+    by_cases hP : P
+    · apply hP
+    · contradiction
+  · intro hP
+    intro hnP
+    contradiction
+
 
 example (P Q : Prop) : ¬ (P → Q) ↔ (P ∧ ¬ Q) := by
-  sorry
+  constructor
+  · intro h
+    by_cases hP : P
+    · constructor
+      · apply hP
+      · intro hQ
+        have hPQ : P → Q
+        · intro hP
+          apply hQ
+        contradiction
+    · have hPQ : P → Q
+      · intro hP
+        contradiction
+      · contradiction
+  · intro h
+    intro hPQ
+    obtain ⟨hP, hnQ ⟩ := h
+    have hQ : Q := by
+      apply hPQ
+      apply hP
+    contradiction
 
 example (P : α → Prop) : ¬ (∀ x, P x) ↔ ∃ x, ¬ P x := by
-  sorry
+  constructor
+  · intro h
+    by_cases hP : ∃ x, ¬ P x
+    · apply hP
+    · have h' : (¬ ∃ x, ¬ P x) ↔ ∀ x, P x :=
+      calc
+          (¬ ∃ x, ¬ P x) ↔ ∀ x, ¬ (¬ P x) := by rel [not_exists]
+           _ ↔ ∀ x, P x := by rel [not_not]
+      rw [h'] at hP
+      contradiction
+  · intro h hP
+    obtain ⟨x, hnP⟩ := h
+    apply hnP
+    apply hP
 
 example : (¬ ∀ a b : ℤ, a * b = 1 → a = 1 ∨ b = 1)
     ↔ ∃ a b : ℤ, a * b = 1 ∧ a ≠ 1 ∧ b ≠ 1 :=
-  sorry
+    sorry
 
 example : (¬ ∃ x : ℝ, ∀ y : ℝ, y ≤ x) ↔ (∀ x : ℝ, ∃ y : ℝ, y > x) :=
   sorry
@@ -87,11 +154,20 @@ example : ¬ (∃ m : ℤ, ∀ n : ℤ, m = n + 5) ↔ ∀ m : ℤ, ∃ n : ℤ,
 
 example : ¬ (∀ x : ℝ, x ^ 2 ≥ x) := by
   push_neg
-  sorry
+  use 0.5
+  norm_num
 
 example : ¬ (∃ t : ℝ, t ≤ 4 ∧ t ≥ 5) := by
   push_neg
-  sorry
+  intro t
+  by_cases ht : t < 5
+  · right
+    apply ht
+  · left
+    rw [not_lt] at ht
+    calc
+      4 < 5 := by numbers
+      _ ≤ t := by rel [ht]
 
 example : ¬ Int.Even 7 := by
   dsimp [Int.Even]
@@ -104,7 +180,12 @@ example {p : ℕ} (k : ℕ) (hk1 : k ≠ 1) (hkp : k ≠ p) (hk : k ∣ p) : ¬ 
   sorry
 
 example : ¬ ∃ a : ℤ, ∀ n : ℤ, 2 * a ^ 3 ≥ n * a + 7 := by
-  sorry
+  push_neg
+  intro a
+  use 2 * a ^ 2
+  calc
+    2 * a ^ 3 = 2 * a ^ 2 * a := by ring
+    _ < 2 * a ^ 2 * a + 7 := by extra
 
 example {p : ℕ} (hp : ¬ Prime p) (hp2 : 2 ≤ p) : ∃ m, 2 ≤ m ∧ m < p ∧ m ∣ p := by
   have H : ¬ (∀ (m : ℕ), 2 ≤ m → m < p → ¬m ∣ p)
