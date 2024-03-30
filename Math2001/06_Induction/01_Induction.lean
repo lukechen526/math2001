5d8c29315dd2242ff57049e400c54d2334823100
Yupo "Luke" Chen <799708+lukechen526@users.noreply.github.com>
2024-03-30 17:03:59 +0000

    completed 6.1

--- Math2001/06_Induction/01_Induction.lean
+++ Math2001/06_Induction/01_Induction.lean
@@ -90,16 +90,35 @@ example : forall_sufficiently_large n : ℕ, 2 ^ n ≥ n ^ 2 := by
   intro n hn
   induction_from_starting_point n, hn with k hk IH
   · -- base case
-    sorry
+    numbers
   · -- inductive step
-    sorry
-
+    calc
+      2 ^ (k + 1) = 2 * 2 ^ k := by ring
+      _ ≥ 2 * k ^ 2 := by rel [IH]
+      _ = k ^ 2 + k ^ 2 := by ring
+      _ = k ^ 2 + k * k := by ring
+      _ ≥ k ^ 2 + 4 * k := by rel [hk]
+      _ = k ^ 2 + 2 * k + 2 * k := by ring
+      _ ≥  k ^ 2 + 2 * k + 2 * 4 := by rel [hk]
+      _ = (k + 1) ^ 2 + 7 := by ring
+      _ ≥ (k + 1) ^ 2 := by extra
 
 /-! # Exercises -/
 
 
 example (n : ℕ) : 3 ^ n ≥ n ^ 2 + n + 1 := by
-  sorry
+  simple_induction n with k IH
+  · -- base case
+    numbers
+  · -- inductive step
+    calc
+    3 ^ (k + 1) = 3 * 3 ^ k := by ring
+              _ ≥ 3 * (k ^ 2 + k + 1) := by rel [IH]
+              _ = 3 * k ^ 2 + 3 * k + 3 := by ring
+              _ = 2 * k ^ 2 + (k ^ 2 + 3 * k + 3) := by ring
+              _ = 2 * k ^ 2 + (k ^ 2 + 2 * k + 1) + (k + 1) + 1 := by ring
+              _ = 2 * k ^ 2 + (k + 1) ^ 2 + (k + 1) + 1 := by ring
+              _ ≥ (k + 1) ^ 2 + (k + 1) + 1 := by extra
 
 example {a : ℝ} (ha : -1 ≤ a) (n : ℕ) : (1 + a) ^ n ≥ 1 + n * a := by
   sorry
@@ -124,10 +143,48 @@ example : forall_sufficiently_large n : ℕ, 2 ^ n ≥ n ^ 2 + 4 := by
 
 example : forall_sufficiently_large n : ℕ, 2 ^ n ≥ n ^ 3 := by
   dsimp
-  sorry
+  use 10
+  intro n hn
+  induction_from_starting_point n, hn with k hk IH
+  · -- base case
+    numbers
+  · -- inductive step
+    calc
+      2 ^ (k + 1) = 2 * 2 ^ k := by ring
+                _ = 2 ^ k + 2 ^ k := by ring
+                _ ≥ k ^ 3 + k ^ 3 := by rel [IH]
+                _ = k ^ 3 + k * k ^ 2 := by ring
+                _ ≥ k ^ 3 + 10 * k ^ 2 := by rel [hk]
+                _ = k ^ 3 + 3 * k ^ 2 + 7 * k ^ 2 := by ring
+                _ = k ^ 3 + 3 * k ^ 2 + 7 * k * k := by ring
+                _ ≥ k ^ 3 + 3 * k ^ 2 + 7 * 10 * k := by rel [hk]
+                _ = k ^ 3 + 3 * k ^ 2 + 70 * k := by ring
+                _ = k ^ 3 + 3 * k ^ 2 + 3 * k + 67 * k := by ring
+                _ ≥ k ^ 3 + 3 * k ^ 2 + 3 * k + 67 * 10 := by rel [hk]
+                _ = k ^ 3 + 3 * k ^ 2 + 3 * k + 1 + 669 := by ring
+                _ ≥ k ^ 3 + 3 * k ^ 2 + 3 * k + 1 := by extra
+                _ = (k + 1) ^ 3 := by ring
 
 theorem Odd.pow {a : ℕ} (ha : Odd a) (n : ℕ) : Odd (a ^ n) := by
-  sorry
-
+  dsimp [Odd] at *
+  simple_induction n with k IH
+  · -- base case
+    use 0
+    calc
+      a ^ 0 = 1 := by ring
+      _ = 2 * 0 + 1 := by numbers
+  · -- inductive step
+    obtain ⟨p, hp⟩ := ha
+    obtain ⟨q, hq⟩ := IH
+    use 2 * p * q + p + q
+    calc
+      a ^ (k + 1) = a * a ^ k := by ring
+                _ = (2 * p + 1) * a ^ k := by rw [hp]
+                _ = (2 * p + 1) * (2 * q + 1) := by rw [hq]
+                _ = 2 * (2 * p * q + p + q) + 1 := by ring
 theorem Nat.even_of_pow_even {a n : ℕ} (ha : Even (a ^ n)) : Even a := by
-  sorry
+  by_contra h
+  rw [← Nat.odd_iff_not_even] at h
+  have ha' := Odd.pow h n
+  rw [Nat.odd_iff_not_even] at ha'
+  contradiction
