10ee497f5d8b270542d61e0eb0f2eac310331b9c
Yupo "Luke" Chen <799708+lukechen526@users.noreply.github.com>
2025-01-20 16:52:36 +0000

    updated 8.1

--- Math2001/09_Sets/01_Sets.lean
+++ Math2001/09_Sets/01_Sets.lean
@@ -129,10 +129,13 @@ example : 4 ∈ {a : ℚ | a < 3} := by
   sorry
 
 example : 4 ∉ {a : ℚ | a < 3} := by
-  sorry
+  dsimp
+  numbers
 
 example : 6 ∈ {n : ℕ | n ∣ 42} := by
-  sorry
+  dsimp
+  use 7
+  numbers
 
 example : 6 ∉ {n : ℕ | n ∣ 42} := by
   sorry
@@ -142,24 +145,39 @@ example : 8 ∈ {k : ℤ | 5 ∣ k} := by
   sorry
 
 example : 8 ∉ {k : ℤ | 5 ∣ k} := by
-  sorry
+  dsimp
+  apply Int.not_dvd_of_exists_lt_and_lt
+  use 1
+  constructor
+  · numbers
+  · numbers
 
 example : 11 ∈ {n : ℕ | Odd n} := by
-  sorry
+  dsimp
+  use 5
+  numbers
 
 example : 11 ∉ {n : ℕ | Odd n} := by
   sorry
 
 
 example : -3 ∈ {x : ℝ | ∀ y : ℝ, x ≤ y ^ 2} := by
-  sorry
+  dsimp
+  intro y
+  calc -3 ≤ 0 := by numbers
+    _ ≤ y ^ 2 := by extra
 
 example : -3 ∉ {x : ℝ | ∀ y : ℝ, x ≤ y ^ 2} := by
   sorry
 
 
 example : {a : ℕ | 20 ∣ a} ⊆ {x : ℕ | 5 ∣ x} := by
-  sorry
+  dsimp [Set.subset_def]
+  intro a ha
+  obtain ⟨k, hk⟩ := ha
+  use 4 * k
+  calc a = 20 * k := hk
+    _ = 5 * (4 * k) := by ring
 
 example : {a : ℕ | 20 ∣ a} ⊈ {x : ℕ | 5 ∣ x} := by
   sorry
@@ -169,16 +187,44 @@ example : {a : ℕ | 5 ∣ a} ⊆ {x : ℕ | 20 ∣ x} := by
   sorry
 
 example : {a : ℕ | 5 ∣ a} ⊈ {x : ℕ | 20 ∣ x} := by
-  sorry
+  dsimp [Set.subset_def]
+  push_neg
+  use 5
+  constructor
+  · use 1
+    ring
+  · apply Nat.not_dvd_of_exists_lt_and_lt
+    use 0
+    constructor
+    · numbers
+    · numbers
+
 
 example : {r : ℤ | 3 ∣ r} ⊆ {s : ℤ | 0 ≤ s} := by
   sorry
 
 example : {r : ℤ | 3 ∣ r} ⊈ {s : ℤ | 0 ≤ s} := by
-  sorry
+  dsimp [Set.subset_def]
+  push_neg
+  use -3
+  constructor
+  · use -1
+    ring
+  · numbers
 
 example : {m : ℤ | m ≥ 10} ⊆ {n : ℤ | n ^ 3 - 7 * n ^ 2 ≥ 4 * n} := by
-  sorry
+  dsimp [Set.subset_def]
+  intro m hm
+  calc
+    m ^ 3 - 7 * m ^ 2 = m * m * (m - 7) := by ring
+    _ ≥ m * m * (10 - 7) := by rel [hm]
+    _ ≥ m * 10 * (10 - 7) := by rel [hm]
+    _ = m * 10 * 3 := by ring
+    _ = 30 * m := by ring
+    _ = 4 * m + 26 * m := by ring
+    _ ≥ 4 * m + 26 * 10 := by rel [hm]
+    _ = 4 * m + 260 := by ring
+    _ ≥ 4 * m := by extra
 
 example : {m : ℤ | m ≥ 10} ⊈ {n : ℤ | n ^ 3 - 7 * n ^ 2 ≥ 4 * n} := by
   sorry
