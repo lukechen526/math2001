a1bac24dd72c684577ea825b26bd45d253421fec
Yupo (Luke) Chen <799708+lukechen526@users.noreply.github.com>
2025-06-08 00:40:09 +0000

    completed problems 1-3

--- Math2001/Homework/hw7.lean
+++ Math2001/Homework/hw7.lean
@@ -16,21 +16,82 @@ for clearer statements and any special instructions. -/
 
 @[autogradedProof 5]
 theorem problem1 (P Q : Prop) : ¬ (P → Q) ↔ (P ∧ ¬ Q) := by
-  sorry
+  constructor
+  · intro h
+    by_cases hP : P
+    · constructor
+      · exact hP
+      · intro hQ
+        apply h
+        intro hP_proof
+        exact hQ
+    · exfalso
+      apply h
+      intro hP_proof
+      have h_neg_p := hP
+      contradiction
+  · intro h
+    obtain ⟨hP, hnQ⟩ := h
+    intro hPQ
+    apply hnQ
+    apply hPQ
+    apply hP
 
 @[autogradedProof 3]
 theorem problem2 : ¬ (∀ x : ℚ, 2 * x ^ 2 ≥ x) := by
   push_neg
-  sorry
+  use 1/4
+  numbers
 
 @[autogradedProof 4]
 theorem problem3 (n : ℕ) : 6 ^ n ≡ 1 [ZMOD 7] ∨ 6 ^ n ≡ 6 [ZMOD 7] := by
-  sorry
+  two_step_induction n with k IH1 IH2
+  · left
+    numbers
+  · right
+    numbers
+  · obtain h | h := IH2
+    · right
+      calc
+        6 ^ (k + 2) = 6 * 6 ^ (k + 1) := by rw [pow_succ]
+        _ ≡ 6 * 1 [ZMOD 7] := by rel [h]
+        _ = 6 := by ring
+    · left
+      calc
+        6 ^ (k + 2) = 6 * 6 ^ (k + 1) := by rw [pow_succ]
+        _ ≡ 6 * 6 [ZMOD 7] := by rel [h]
+        _ = 36 := by numbers
+        _ ≡ 1 + 7 * 5 [ZMOD 7] := by numbers
+        _ ≡ 1 [ZMOD 7] := by extra
 
 @[autogradedProof 4]
 theorem problem4 (n : ℕ) :
     4 ^ n ≡ 1 [ZMOD 7] ∨ 4 ^ n ≡ 2 [ZMOD 7] ∨ 4 ^ n ≡ 4 [ZMOD 7] := by
-  sorry
+  match n with
+  | 0 => left; numbers
+  | 1 => right; right; numbers
+  | 2 =>
+    right
+    left
+    calc
+      4 ^ 2 ≡ 16 [ZMOD 7] := by numbers
+      _ ≡ 2 + 7 * 2 [ZMOD 7] := by numbers
+      _ ≡ 2 [ZMOD 7] := by extra
+  | k + 3 =>
+    have IH := problem4 k
+    rw [pow_add, pow_three]
+    obtain h | h | h := IH
+    · left
+      rel [h]
+      numbers
+    · right
+      right
+      rel [h]
+      numbers
+    · right
+      left
+      rel [h]
+      numbers
 
 @[autogradedProof 5]
 theorem problem5 {a : ℝ} (ha : -1 ≤ a) : ¬ ∃ n : ℕ, (1 + a) ^ n < 1 + n * a := by
